4dc03971d0a4b7682bb1f8fe613e6640c01a6427
cheol2024 <cheolfront2024@gmail.com>
2024-11-30 22:31:13 +0900

    ver 4

--- components/home/animation.js
+++ components/home/animation.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import Lottie from 'react-lottie-player'
+import dynamic from 'next/dynamic';
+import React from 'react';
 
-import lottieJson from '/public/animation.json'
+// Lottie 컴포넌트를 클라이언트에서만 동적으로 로드
+const Lottie = dynamic(() => import('react-lottie-player'), {
+  ssr: false, // 서버 측 렌더링 비활성화
+});
+
+import lottieJson from '/public/animation.json';
 
 export default function Animation() {
   return (
@@ -9,7 +14,7 @@ export default function Animation() {
       loop
       animationData={lottieJson}
       play
-      style={{ width: `100%`, height: `100%` }}
+      style={{ width: '100%', height: '100%' }}
     />
-  )
+  );
 }
